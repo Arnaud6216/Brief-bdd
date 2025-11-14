843246bad09071f015da25a61b14aaa349b25076
Guevaer Arnaud <guevaer8@gmail.com>
2025-11-14 18:29:53 +0100

    add second sql script with bonus

---
+++ 2_count_adress_by_commune.sql
@@ -0,0 +1,17 @@
+SELECT c.nom_commune, COUNT(*) AS nombre_adresse
+FROM adresse a
+JOIN commune c ON a.id_commune = c.code_insee
+GROUP BY c.nom_commune
+ORDER BY nom_commune ASC;
+
+
+
+-- bonus : par voie
+
+SELECT c.nom_commune, v.nom_voie, COUNT(*) AS nombre_adresse
+FROM adresse a
+JOIN commune c ON a.id_commune = c.code_insee
+LEFT JOIN voie v ON a.id_voie = v.id_fantoir
+GROUP BY c.nom_commune, v.nom_voie
+ORDER BY c.nom_commune, v.nom_voie;
+
